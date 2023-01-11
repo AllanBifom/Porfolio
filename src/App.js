93803a94a16e5b82cc99e1d681cb873422928f59
Allan Bifom (aeb672) <aeb672@usask.ca>
2023-01-11 17:12:39 -0600

    finished nav bar and css breaking now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {NavBar} from './componets/NavBar';
+import {HomeBanner} from './componets/HomeBanner';
+import {Skills} from './componets/Skills'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <HomeBanner />
     </div>
   );
 }
